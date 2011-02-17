98ab3fd8dd2eb82975f7abd54030f1a223d8e9fd
brahn@fushni.com <brahn@fushni.com@0e4ad543-c941-097b-b1b4-342b9fca981c>
2011-02-17 09:39:56 +0000

    ADDED: Project - This directory contains all the source for the AccessoryMenu.addin ADDED: Addin - Contains the compiled binary and resources needed for the addin.
    The project is configured to copy the files from .\Addin\ to c:\clarion7\accessory\addins using relative paths so you will have to adjust these to suit your environment

---
+++ trunk/Project/AccessoryMenu/Src/MenuOptionsPanel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop;
+using ICSharpCode.SharpDevelop.Gui;
+
+namespace Clarion.AccessoryMenu
+{
+    public partial class MenuOptionsPanel : AbstractOptionPanel
+    {
+        public MenuOptionsPanel()
+        {
+            InitializeComponent();
+        }
+
+        public override void LoadPanelContents()
+        {
+            if (PropertyService.Get("Clarion.AccessoryMenu.MenuOptionsPanel.OptionSelected") == "")
+            {
+                LoggingService.Debug("tesT");
+                PropertyService.Set("Clarion.AccessoryMenu.MenuOptionsPanel.OptionSelected", "1");
+            }
+            int optionSelected = PropertyService.Get("Clarion.AccessoryMenu.MenuOptionsPanel.OptionSelected", 1);
+            switch (optionSelected)
+            {
+                case 1:
+                    radioButton1.Checked = true;
+                    break;
+                case 2:
+                    radioButton2.Checked = true;
+                    break;
+                case 3:
+                    radioButton3.Checked = true;
+                    break;
+            }
+        }
+
+        public override bool StorePanelContents()
+        {
+            if (radioButton1.Checked == true)
+                PropertyService.Set("Clarion.AccessoryMenu.MenuOptionsPanel.OptionSelected", "1");
+
+            if (radioButton2.Checked == true)
+                PropertyService.Set("Clarion.AccessoryMenu.MenuOptionsPanel.OptionSelected", "2");
+
+            if (radioButton3.Checked == true)
+                PropertyService.Set("Clarion.AccessoryMenu.MenuOptionsPanel.OptionSelected", "3");
+
+            return true;
+        }
+    }
+}
